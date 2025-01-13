70e529067e10b64fd0a8156cb0f7cc99adcd44fc
Dan <daniel@ada.ac.uk>
2025-01-13 11:55:43 +0000

    folder change

--- Javacript/Incrementer/postdecrement.js
+++
@@ -1,2 +0,0 @@
-let x = 5;
-let y = x--; // y is assigned the current value of x, then x is decremented
---
+++ Javacript/PrePostCrement/postdecrement.js
@@ -0,0 +1,5 @@
+let x = 5;
+let y = x--; // y is assigned the current value of x, then x is decremented
+
+console.log(x); // Output: 4
+console.log(y); // Output: 5
